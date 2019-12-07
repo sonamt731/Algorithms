22be5e5d7f03a07e1c7acb234356219f716ecfd6
Sonam Tailor <srt346@nyu.edu>
2019-12-07 15:29:20 -0500

    BFS

--- bfs.java
+++ bfs.java
@@ -16,3 +16,32 @@ While fronter ! empty
 //Time is theta (|V| + |E|)
 	
 
+//BFS from the textbook is better
+
+BFS(G,s) //s is the head
+
+for each u in G.V - {s} //minus vertex s
+	u.color = white; //undiscovered
+	u.pi = nil; //no parent yet
+	u.d = infinity; //level
+
+s.d = 1; //first level
+s.pi = nil; //no parent
+s.color = grey;
+
+Q=empty; 
+enqueue(Q,s); 
+
+while (Q!= empty){
+	u = dequeue(Q);
+	for v in Adj[u]{
+		if(v.color == white){ //if it undiscovered
+			v.color = grey;
+			v.d = u.d + 1; //level
+			v.pi = u; //parent that discovered it
+			enqueue(Q,v)
+		}
+	}
+
+	u.color = black; //finished exploring neighbors 
+}
